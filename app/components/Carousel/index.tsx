84eff748413c92a8a7069e8e6889bc8411185cda
Chi-Han Huang <huangchihan@gmail.com>
2022-09-04 13:01:38 +0800

    Added product preview border highlight.

--- app/components/Carousel/index.tsx
+++ app/components/Carousel/index.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import { useState, useCallback } from 'react';
 import Slider from 'react-slick';
 import type { Settings } from 'react-slick';
 import type { LinksFunction } from '@remix-run/node';
@@ -19,31 +21,64 @@ interface PicsCarouselProps {
 	images: string[];
 };
 
+/*
+ * - [x] hover thumbnail display border.
+ * - [ ] clicks on thumbnail should display that image.
+ */
 function PicsCarousel({ images }: PicsCarouselProps) {
 	const settings: Settings = {
-		dots: true,
+		dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
 		autoplay: false,
-		//centerMode: true,
 	}
 
+	const [displayImageIdx, setDisplayImageIdx] = useState(1);
+	const handleChangeImage = useCallback((index: number) => {
+		setDisplayImageIdx(index);
+	}, [])
+
 	return (
-		<div className="product-carousel-container">
-			<Slider {...settings}>
-				{
-					images.map((image, index) => {
-						return (
-							<div key={index}>
-								<img className="product-carousel-image" src={image} />
-							</div>
-						)
-					})
-				}
-			</Slider>
-		</div>
+		<>
+			<div className="product-carousel-container">
+				<Slider {...settings}>
+					{
+						images.map((image, index) => {
+							return (
+								<div key={index}>
+									<img className="product-carousel-image" src={image} />
+								</div>
+							)
+						})
+					}
+				</Slider>
+			</div>
+
+			<div className="thumbnails-hover-images-container">
+				<img src={images[displayImageIdx]} />
+				<div className="thumbnails-container">
+					{
+						images.map((image, index) => {
+							return (
+								<div
+									key={index}
+									className={clsx(
+											"product-thumbnail",
+											displayImageIdx === index && 'thumbail-highlight'
+										)
+									}
+									onClick={() => handleChangeImage(index)}
+								>
+									 <img src={image} />
+								</div>
+							);
+						})
+					}
+				</div>
+			</div>
+		</>
 	);
 };
 
