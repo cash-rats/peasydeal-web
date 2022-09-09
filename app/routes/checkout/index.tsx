313dd81c758999a6c0d6be6544062457a47e24f7
Chi-Han Huang <huangchihan@gmail.com>
2022-09-09 17:43:58 +0800

    Progress check.

--- app/routes/checkout/index.tsx
+++ app/routes/checkout/index.tsx
@@ -19,55 +19,68 @@ function CheckoutPage() {
 	return (
 		<section className="checkout-page-container">
 			<div className="left">
+
 				{/* You Details  */}
-				<h1 className="title">
-					Shipping Details
-				</h1>
+				<div className="form-container">
+					<h1 className="title">
+						Shipping Details
+					</h1>
 
-				<div className="shipping-detail-container">
-					<FormControl className="shipping-detail-form">
-						<div className="shipping-form-field">
-							<FormLabel>Email address</FormLabel>
-  						<Input size='lg' type='email' />
-						</div>
-
-						<div className="shipping-form-fields fields--2">
+					<div className="shipping-detail-container">
+						<FormControl className="shipping-detail-form">
 							<div className="shipping-form-field">
-								<FormLabel>First name (required) </FormLabel>
+								<FormLabel>Email address</FormLabel>
+  							<Input size='lg' type='email' />
+							</div>
+
+							<div className="shipping-form-fields fields--2">
+								<div className="shipping-form-field">
+									<FormLabel>First name (required) </FormLabel>
+  								<Input size='lg' type='email' />
+								</div>
+
+								<div className="shipping-form-field">
+									<FormLabel>Last name (required) </FormLabel>
+  								<Input size='lg' type='email' />
+								</div>
+							</div>
+
+							<div className="shipping-form-field">
+								<FormLabel>Address line 1 (required)</FormLabel>
   							<Input size='lg' type='email' />
 							</div>
 
 							<div className="shipping-form-field">
-								<FormLabel>Last name (required) </FormLabel>
-  							<Input size='lg' type='email' />
-							</div>
-						</div>
-
-						<div className="shipping-form-field">
-							<FormLabel>Address line 1 (required)</FormLabel>
-  						<Input size='lg' type='email' />
-						</div>
-
-						<div className="shipping-form-field">
-							<FormLabel>Address line 2</FormLabel>
-  						<Input size='lg' type='email' />
-						</div>
-
-						<div className="shipping-form-fields fields--2">
-							<div className="shipping-form-field">
-								<FormLabel> Postal </FormLabel>
+								<FormLabel>Address line 2</FormLabel>
   							<Input size='lg' type='email' />
 							</div>
 
-							<div className="shipping-form-field">
-								<FormLabel> City </FormLabel>
-  							<Input size='lg' type='email' />
+							<div className="shipping-form-fields fields--2">
+								<div className="shipping-form-field">
+									<FormLabel> Postal </FormLabel>
+  								<Input size='lg' type='email' />
+								</div>
+
+								<div className="shipping-form-field">
+									<FormLabel> City </FormLabel>
+  								<Input size='lg' type='email' />
+								</div>
 							</div>
-						</div>
-					</FormControl>
+						</FormControl>
+					</div>
 				</div>
 
 				{/*Payment gateways*/}
+				{/* You Details  */}
+				<div className="form-container">
+					<h1 className="title">
+						Pay with
+					</h1>
+
+					{/* select payment gateways */}
+
+
+				</div>
 			</div>
 
 			<div className="right">
