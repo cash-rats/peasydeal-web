0b2e3fc935b0e9f276dd48c96879809eff7b3f1c
Chi-Han Huang <huangchihan@gmail.com>
2022-09-11 11:33:08 +0800

    Added supported icon creditcard icons.

--- app/routes/checkout/index.tsx
+++ app/routes/checkout/index.tsx
@@ -1,95 +1,113 @@
 import { LinksFunction } from '@remix-run/node';
 import {
-	FormControl,
-	FormLabel,
-	FormErrorMessage,
-	Input,
-	FormHelperText,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  FormHelperText,
 } from '@chakra-ui/react';
 import TextField from '@mui/material/TextField';
 
+import visa from './images/visa.png';
+import mastercard from './images/mastercard.png';
+import amex from './images/american-express.png';
 import styles from './styles/Checkout.css';
 
+console.log('visa png', visa);
+
 export const links: LinksFunction = () => {
-	return [
-		{ rel: 'stylesheet', href: styles },
-	];
+  return [
+    { rel: 'stylesheet', href: styles },
+  ];
 };
 
 function CheckoutPage() {
-	return (
-		<section className="checkout-page-container">
-			<div className="left">
+  return (
+    <section className="checkout-page-container">
+      <div className="left">
 
-				{/* You Details  */}
-				<div className="form-container">
-					<h1 className="title">
-						Shipping Details
-					</h1>
+        {/* You Details  */}
+        <div className="form-container">
+          <h1 className="title">
+            Shipping Details
+          </h1>
 
-					<div className="shipping-detail-container">
-						<FormControl className="shipping-detail-form">
-							<div className="shipping-form-field">
-								<FormLabel>Email address</FormLabel>
-  							<Input size='lg' type='email' />
-							</div>
+          <div className="shipping-detail-container">
+            <FormControl className="shipping-detail-form">
+              <div className="shipping-form-field">
+                <FormLabel>Email address</FormLabel>
+                <Input size='lg' type='email' />
+              </div>
 
-							<div className="shipping-form-fields fields--2">
-								<div className="shipping-form-field">
-									<FormLabel>First name (required) </FormLabel>
-  								<Input size='lg' type='email' />
-								</div>
+              <div className="shipping-form-fields fields--2">
+                <div className="shipping-form-field">
+                  <FormLabel>First name (required) </FormLabel>
+                  <Input size='lg' type='email' />
+                </div>
 
-								<div className="shipping-form-field">
-									<FormLabel>Last name (required) </FormLabel>
-  								<Input size='lg' type='email' />
-								</div>
-							</div>
+                <div className="shipping-form-field">
+                  <FormLabel>Last name (required) </FormLabel>
+                  <Input size='lg' type='email' />
+                </div>
+              </div>
 
-							<div className="shipping-form-field">
-								<FormLabel>Address line 1 (required)</FormLabel>
-  							<Input size='lg' type='email' />
-							</div>
+              <div className="shipping-form-field">
+                <FormLabel>Address line 1 (required)</FormLabel>
+                <Input size='lg' type='email' />
+              </div>
 
-							<div className="shipping-form-field">
-								<FormLabel>Address line 2</FormLabel>
-  							<Input size='lg' type='email' />
-							</div>
+              <div className="shipping-form-field">
+                <FormLabel>Address line 2</FormLabel>
+                <Input size='lg' type='email' />
+              </div>
 
-							<div className="shipping-form-fields fields--2">
-								<div className="shipping-form-field">
-									<FormLabel> Postal </FormLabel>
-  								<Input size='lg' type='email' />
-								</div>
+              <div className="shipping-form-fields fields--2">
+                <div className="shipping-form-field">
+                  <FormLabel> Postal </FormLabel>
+                  <Input size='lg' type='email' />
+                </div>
 
-								<div className="shipping-form-field">
-									<FormLabel> City </FormLabel>
-  								<Input size='lg' type='email' />
-								</div>
-							</div>
-						</FormControl>
-					</div>
-				</div>
+                <div className="shipping-form-field">
+                  <FormLabel> City </FormLabel>
+                  <Input size='lg' type='email' />
+                </div>
+              </div>
+            </FormControl>
+          </div>
+        </div>
 
-				{/*Payment gateways*/}
-				{/* You Details  */}
-				<div className="form-container">
-					<h1 className="title">
-						Pay with
-					</h1>
+        {/*Payment gateways*/}
+        <h3 className="title">
+          Payment
+        </h3>
+        {/* You Details  */}
+        <div className='payment-form-container'>
+          <span> We accept the following card types </span>
+          <ul className='card-types'>
+            <li>
+              <img alt='visa' src={visa} />
+            </li>
 
-					{/* select payment gateways */}
-					<TextField variant='outlined' />
+            <li>
+              <img alt='mastercard' src={mastercard} />
+            </li>
 
+            <li>
+              <img alt='americanexpress' src={amex} />
+            </li>
+          </ul>
 
-				</div>
-			</div>
+          <form className="payment-form">
 
-			<div className="right">
+          </form>
+        </div>
+      </div>
 
-			</div>
-		</section>
-	);
+      <div className="right">
+
+      </div>
+    </section>
+  );
 }
 
 export default CheckoutPage;
