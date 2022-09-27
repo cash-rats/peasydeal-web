aa15e048dc5ed4982f8ad2b9b9ded95df2b3c688
Chi-Han Huang <huangchihan@gmail.com>
2022-09-27 17:28:07 +0800

    Progress check on accordion.

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import type { ReactNode } from 'react';
 import type { LinksFunction } from '@remix-run/node';
 import {
   TextField,
 } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
-import { BiChevronDown } from 'react-icons/bi';
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
 
 import styles from './styles/Footer.css';
 import Amex from './images/amex.svg';
@@ -13,6 +14,7 @@ import Discover from './images/pi_discover.svg';
 import GooglePay from './images/google_pay.svg';
 import MasterCard from './images/pi_master.svg';
 import Visa from './images/visa.svg';
+import clsx from 'clsx';
 
 export const links: LinksFunction = () => {
   return [
@@ -20,21 +22,88 @@ export const links: LinksFunction = () => {
   ];
 };
 
+type PanelStatus = {
+  [index: string]: boolean;
+  panel1: boolean;
+  panel2: boolean;
+  panel3: boolean;
+  panel4: boolean;
+}
+
+interface FooterListTitleProps {
+  onClick: () => void;
+  isActive: boolean;
+  title: ReactNode | string;
+}
+
+// @doc: Accordion animation https://codepen.io/felipefialho/pen/AwYmMe
+function FooterListTitle({ onClick, isActive = false, title }: FooterListTitleProps) {
+  return (
+    <div
+      className="footer-list-title"
+      onClick={onClick}
+    >
+      <h3>
+        {title}
+      </h3>
+      <span>
+        {
+          isActive
+            ? <BiChevronUp />
+            : <BiChevronDown />
+        }
+
+      </span>
+    </div>
+  );
+}
+
+interface FooterListContentProps {
+  children: ReactNode;
+  isActive: boolean;
+}
+
+// TODO: refactor transition mechanism.
+function FooterListContent({ children, isActive }: FooterListContentProps) {
+  return (
+    <div
+      className={clsx("footer-list-content", {
+        "footer-item-grow": isActive,
+      }
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
 function Footer() {
+  const [panelStatus, setPanelStatus] = useState<PanelStatus>({
+    panel1: false,
+    panel2: false,
+    panel3: false,
+    panel4: false,
+  });
+
   return (
     <footer className="footer-container">
       <ul className="footer-list">
-        <li className="footer-list-panel">
-          <div className="footer-list-title">
-            <h3>
-              Subscribe
-            </h3>
-            <span>
-              <BiChevronDown />
-            </span>
-          </div>
-          <div className="list-content">
-            <p>
+        <li
+          id="panel1"
+          className="footer-list-panel"
+        >
+          <FooterListTitle
+            onClick={() => {
+              setPanelStatus({
+                ...panelStatus,
+                panel1: !panelStatus.panel1,
+              });
+            }}
+            isActive={panelStatus.panel1}
+            title="Subscribe"
+          />
+          <FooterListContent isActive={panelStatus['panel1']}>
+            <p className="desc">
               Enter your email below to be the first to know about new collections and product launches.
             </p>
 
@@ -56,46 +125,62 @@ function Footer() {
                 }}
               />
             </div>
-          </div>
+          </FooterListContent>
         </li>
 
         <li>
-          <div className="footer-list-title">
-            <h3>
-              Policy
-            </h3>
-            <span>
-              <BiChevronDown />
-            </span>
-          </div>
-          <div className="list-content">
-          </div>
+          <FooterListTitle
+            onClick={() => {
+              setPanelStatus({
+                ...panelStatus,
+                panel2: !panelStatus.panel2,
+              });
+            }}
+            isActive={panelStatus.panel2}
+            title="Policy"
+          />
+
+          <FooterListContent isActive={panelStatus.panel2}>
+            Privacy Policy
+            Return Policy
+            Shipping Policy
+            Terms of Use
+          </FooterListContent>
         </li>
 
         <li>
-          <div className="footer-list-title">
-            <h3>
-              Service
-            </h3>
-            <span>
-              <BiChevronDown />
-            </span>
-          </div>
-          <div className="list-content">
-          </div>
+          <FooterListTitle
+            onClick={() => {
+              setPanelStatus({
+                ...panelStatus,
+                panel3: !panelStatus.panel3,
+              });
+            }}
+            isActive={panelStatus.panel3}
+            title="Service"
+          />
+          <FooterListContent isActive={panelStatus.panel3}>
+            About Us
+            How to Personalised
+          </FooterListContent>
         </li>
 
         <li>
-          <div className="footer-list-title">
-            <h3>
-              Contact Us
-            </h3>
-            <span>
-              <BiChevronDown />
-            </span>
-          </div>
-          <div className="list-content">
-          </div>
+          <FooterListTitle
+            onClick={() => {
+              setPanelStatus({
+                ...panelStatus,
+                panel4: !panelStatus.panel4,
+              });
+            }}
+            isActive={panelStatus.panel4}
+            title="Contact Us"
+          />
+          <FooterListContent isActive={panelStatus.panel4}>
+            <p className="contact-email">
+              contact@peasydeal.com
+            </p>
+          </FooterListContent>
         </li>
       </ul>
 
