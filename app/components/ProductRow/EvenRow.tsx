bad5246748d6af47034b6a0f88781dbbd3ad5a31
Chi-Han Huang <huangchihan@gmail.com>
2022-11-04 17:00:22 +0800

    Minor tweak.

--- app/components/ProductRow/EvenRow.tsx
+++ app/components/ProductRow/EvenRow.tsx
@@ -38,6 +38,7 @@ export default function EvenRow({
 							<MediumGridSkeleton />
 							<MediumGridSkeleton />
 							<MediumGridSkeleton />
+							<MediumGridSkeleton />
 						</>
 					)
 					: (
