10068d96242aa9dd7c7ad9b112a937029416da77
Chi-Han Huang <huangchihan@gmail.com>
2022-12-01 11:02:30 +0800

    Debug log.

--- app/utils/stripe.server.tsx
+++ app/utils/stripe.server.tsx
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 
 const STRIPE_PRIVATE_KEY = process.env.STRIPE_PRIVATE_KEY || '';
+console.log('debug STRIPE_PRIVATE_KEY', STRIPE_PRIVATE_KEY)
 const stripe = new Stripe(STRIPE_PRIVATE_KEY, {
   apiVersion: '2022-08-01'
 });
