cb72352e671522d95e0e1e7bd8192bbd0e12a841
Chi-Han Huang <huangchihan@gmail.com>
2022-12-15 18:19:55 +0800

    Minor tweak on 404 css.

--- app/components/404/index.tsx
+++ app/components/404/index.tsx
@@ -13,38 +13,37 @@ export const links: LinksFunction = () => {
 
 export default function NotFound() {
   return (
-    <div className="h-[40rem]">
-      <div
-        className="relative overflow-hidden h-screen bg-bbefef bg-center bg-no-repeat"
-        style={{ backgroundImage: `url(${BGBottom})` }}
-      >
-        <div className="flex mt-10 flex-col">
-          {/* title */}
-          <div
-            className="overflow-hidden h-[193px] bg-center bg-no-repeat"
-            style={{ backgroundImage: `url(${BGTop})` }}
-          />
+    <div className="relative h-[40rem]">
+      <div className="flex mt-10 flex-col">
+        {/* title */}
+        <div
+          className="overflow-hidden h-[193px] bg-center bg-no-repeat"
+          style={{ backgroundImage: `url(${BGTop})` }}
+        />
 
-          {/* indication */}
-          <div className="text-sm text-[#406455] font-[500] leading-normal text-center z-100 ">
-            {/* power line */}
-            <div className="text-2xl mb-2 text-[#007e62] leading-3">
-              Oops, how'd you get here?!
-            </div>
+        {/* indication */}
+        <div className="text-sm text-[#406455] font-[500] leading-normal text-center z-100 ">
+          {/* power line */}
+          <div className="text-2xl mb-2 text-[#007e62] leading-3">
+            Oops, how'd you get here?!
+          </div>
 
-            <div> Something went wrong,</div>
+          <div> Something went wrong,</div>
 
-            <div>return to&nbsp;
-              <span>
-                Home Page
-              </span>
-            </div>
+          <div>return to&nbsp;
+            <span>
+              Home Page
+            </span>
           </div>
         </div>
-
-        <div className="PageNotFound__mountain" style={{ backgroundImage: `url(${MountainSprite})` }} />
       </div>
 
+      <div className="PageNotFound__mountain" style={{ backgroundImage: `url(${MountainSprite})` }} />
+
+      <div
+        className="absolute w-[100%] h-[200px] bottom-0 left-0 translate-y-[-50%] overflow-hidden  bg-bbefef bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${BGBottom})` }}
+      />
     </div>
   );
 }
