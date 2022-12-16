5d1f8b44772bbb571f8da4914dc0d98577f5843c
Chi-Han Huang <huangchihan@gmail.com>
2022-12-16 13:12:15 +0800

    Added background image for global 404 page.

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -2,6 +2,7 @@ import type { LinksFunction, LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useLoaderData, useFetcher, Form } from '@remix-run/react';
 
+import CategoryContext from '~/context/categories';
 import FourOhFour, { links as FourOhFourLinks } from '~/components/FourOhFour';
 import Header, { links as HeaderLinks } from '~/components/Header';
 import Footer, { links as FooterLinks } from '~/components/Footer';
@@ -42,25 +43,27 @@ function GlobalSplatFourOhFour() {
   }
 
   return (
-    <div className="pt-48">
-      <Form action='/search'>
-        <Header
-          categoriesBar={
-            <CategoriesNav categories={categories} />
-          }
+    <div className="pt-48 bg-center bg-cover bg-no-repeat bg-home-gradient-light-sm md:pt-40 md:bg-home-gradient-light">
+      <CategoryContext.Provider value={categories}>
+        <Form action='/search'>
+          <Header
+            categoriesBar={
+              <CategoriesNav categories={categories} />
+            }
 
-          searchBar={
-            <DropDownSearchBar
-              placeholder='Search products by name'
-              onDropdownSearch={searchSuggests}
-              results={suggests}
-              onSearch={handleSearch}
-            />
-          }
-          numOfItemsInCart={numOfItemsInCart}
-          onSearch={handleSearch}
-        />
-      </Form>
+            searchBar={
+              <DropDownSearchBar
+                placeholder='Search products by name'
+                onDropdownSearch={searchSuggests}
+                results={suggests}
+                onSearch={handleSearch}
+              />
+            }
+            numOfItemsInCart={numOfItemsInCart}
+            onSearch={handleSearch}
+          />
+        </Form>
+      </CategoryContext.Provider>
 
       <FourOhFour />
 
