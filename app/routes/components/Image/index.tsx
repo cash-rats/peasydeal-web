bb9b37e72dad75e8b9f6aa6216bedfd2d1a95a88
Chi-Han Huang <huangchihan@gmail.com>
2023-02-15 14:21:34 +0800

    Progress check.

--- app/routes/components/Image/index.tsx
+++ app/routes/components/Image/index.tsx
@@ -1,6 +1,17 @@
-import type { LinksFunction } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
 
-export const link: Function = () => { };
+export const loader: LoaderFunction = ({ request }) => { 
+  // Receive image src url, width and height. 
+  const url = new URL(request.url);
+  const src = url.searchParams.get('src');
+  const width = url.searchParams.get('w');
+  const height = url.searchParams.get('h');
+  const quality = url.searchParams.get('')
+  
+  
+  
+  return null
+};
 
 function Image() {
 
