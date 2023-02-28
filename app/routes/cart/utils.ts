6e54e515a45c1022349f7605dbad6469cbb2fcda
Chi-Han Huang <huangchihan@gmail.com>
2023-02-28 23:51:38 +0800

    Update renew retail price value.

--- app/routes/cart/utils.ts
+++ app/routes/cart/utils.ts
@@ -20,6 +20,10 @@ export const syncShoppingCartWithNewProductsInfo = (
         {
           ...cart[prod.variation_uuid],
           discountReason: prod.discount_reason,
+
+          // If `origin_unit_price` and `discounted_price` we got from '/data-server/ec/v1/accountant/order-amount'.
+          // are equal, we don't update the `retailPrice` but use `retailPrice` value from shopping cart.
+          // or else, we update the retailPrice coming from newly purchased products.
           retailPrice: prod.origin_unit_price.toString() === prod.discounted_price.toString()
             ? itemInCart.retailPrice.toString()
             : prod.origin_unit_price.toString(),
