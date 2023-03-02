0b47f0636b67594f2e21afb546fabf2c249507f1
Chi-Han Huang <huangchihan@gmail.com>
2023-03-02 18:47:10 +0800

    Added structured data for category products.

--- app/routes/__index/$collection/structured_data.ts
+++ app/routes/__index/$collection/structured_data.ts
@@ -2,10 +2,14 @@ import type {
   WithContext,
   BreadcrumbList,
   ListItem,
+  Product as ProductSchema,
 } from 'schema-dts';
 import type { StructuredDataFunction } from 'remix-utils';
 
 import { getCanonicalDomain } from '~/utils/seo';
+import { composeProductDetailURL } from '~/utils';
+
+import type { Product } from '~/shared/types';
 
 import type { LoaderDataType } from './types';
 
@@ -40,12 +44,40 @@ const composeBreadcrumbList = (baseURL: string, pathname: string): ListItem[] =>
   return listItems;
 };
 
+const composeProductList = (prods: Product[]): WithContext<ProductSchema>[] => {
+  const prodsSchemas: WithContext<ProductSchema>[] = [];
+
+  for (const prod of prods) {
+    prodsSchemas.push({
+      "@context": "https://schema.org",
+      "@type": "Product",
+      "name": prod.title,
+      "image": prod.main_pic,
+      "description": prod.description,
+      "sku": prod.sku,
+      "offers": {
+        "@type": "Offer",
+        "price": prod.salePrice.toString(),
+        "priceCurrency": prod.currency,
+        "availability": "https://schema.org/InStock",
+        "url": composeProductDetailURL({
+          productName: prod.title,
+          variationUUID: prod.productUUID,
+        })
+      }
+    });
+  }
+
+  return prodsSchemas;
+}
+
 const structuredData: StructuredDataFunction = ({
   data,
   params,
   location,
 }) => {
   const loaderData: LoaderDataType = data;
+
   const { collection } = params;
 
   if (
@@ -65,7 +97,9 @@ const structuredData: StructuredDataFunction = ({
     ),
   }
 
-  return [breadcrumbList];
+  const productList = composeProductList(loaderData.products);
+
+  return [breadcrumbList, ...productList];
 };
 
 export default structuredData;
