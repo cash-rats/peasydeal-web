4aceffeec2f3e74a0394a4f42a277148eeba99f3
Bblurock <>
2023-05-07 18:39:50 +0800

    Update email subscription modal style

--- app/components/EmailSubscribeModal/index.tsx
+++ app/components/EmailSubscribeModal/index.tsx
@@ -9,6 +9,7 @@ import {
 } from '@chakra-ui/react'
 
 import type { ApiErrorResponse } from '~/shared/types';
+import imageBg from './images/email_subscription.png'
 
 interface SubscribeModalParams {
   open: boolean;
@@ -34,17 +35,19 @@ function SubscribeModal({ open, onClose, error }: SubscribeModalParams) {
 
         <ModalBody>
           <div className="pb-4">
-            <p className="font-poppins text-base">
+            <div className="font-poppins text-base text-center justify-center gap-2">
               {
                 error !== null
                   ? 'Something went wrong! Please check the email your entered and try again.'
-                  : (<>
+                  : (
+                    <>
+                    <img src={imageBg} alt="email subscribe successfull" className='mx-auto mb-2'/>
                     <p>An confirmation link and coupon has send to your email.</p>
                     <br/>
-                    <p>Please check your email for $3 voucher code and click the "Confirm & Validate" button in the email to activate your voucher.</p>
+                    <p>Please check your email for <b>£3 GBP voucher code</b> and click the <b>Confirm & Validate</b> button in the email to activate your voucher.</p>
                   </>)
               }
-            </p>
+            </div>
 
             <div className="flex justify-end  mt-4">
               <Button onClick={onClose}>
