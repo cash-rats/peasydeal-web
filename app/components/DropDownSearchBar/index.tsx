c2bd62e61c6caf1c9e3162b2ed20236460982596
Chi-Han Huang <huangchihan@gmail.com>
2023-05-20 01:38:02 +0800

    Remove unused code.

--- app/components/DropDownSearchBar/index.tsx
+++ app/components/DropDownSearchBar/index.tsx
@@ -1,21 +1,13 @@
-import { Fragment, useMemo } from 'react';
-import type { OnSubmitParams } from '@algolia/autocomplete-core'
+import { useMemo } from 'react';
 import type { LinksFunction } from '@remix-run/node';
 import autocompleteThemeClassicStyles from '@algolia/autocomplete-theme-classic/dist/theme.min.css';
-import type { AutocompleteComponents } from '@algolia/autocomplete-shared';
-import { createQuerySuggestionsPlugin } from '@algolia/autocomplete-plugin-query-suggestions';
-import { createLocalStorageRecentSearchesPlugin } from '@algolia/autocomplete-plugin-recent-searches';
-import { createAutocomplete } from '@algolia/autocomplete-core';
-import insightsClient from 'search-insights';
 
-import { ALGOLIA_INDEX_NAME } from '~/utils/get_env_source';
 import { searchClient } from '~/components/Algolia';
 import {
   createProductsSuggestionsPlugin,
   createCategoriesPlugin,
   createRecentSearchPlugin,
 } from '~/components/Algolia/plugins';
-import type { AlgoliaIndexItem } from '~/components/Algolia/types';
 
 import Autocomplete from './Autocomplete';
 import DropDownSearchBarStyles from './styles/DropDownSearchBar.css';
@@ -26,32 +18,6 @@ export const links: LinksFunction = () => {
     { rel: 'stylesheet', href: DropDownSearchBarStyles },
   ];
 }
-
-// const recentSearchPlugin = createLocalStorageRecentSearchesPlugin({
-//   key: 'products-recent-search',
-//   limit: 3,
-//   transformSource({ source }) {
-//     return {
-//       ...source,
-//       templates: {
-//         ...source.templates,
-//         header({ state }) {
-//           if (state.query) {
-//             return null;
-//           }
-
-//           return (
-//             <Fragment>
-//               <span className="aa-SourceHeaderTitle">Your searches</span>
-//               <div className="aa-SourceHeaderLine" />
-//             </Fragment>
-//           );
-//         },
-//       },
-//     }
-//   },
-// });
-
 
 function DropDownSearchBar() {
   const recentSearchPlugin = useMemo(() => {
