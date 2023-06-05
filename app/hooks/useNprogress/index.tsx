859d56331147f2f4cf70a7d658e2516dcad23b2c
Chi-Han Huang <huangchihan@gmail.com>
2023-06-05 16:18:02 +0800

    transition ---> navigation.

--- app/hooks/useNprogress/index.tsx
+++ app/hooks/useNprogress/index.tsx
@@ -1,6 +1,6 @@
 import type { LinksFunction } from '@remix-run/node';
 import { useEffect } from 'react';
-import { useTransition } from '@remix-run/react';
+import { useNavigation } from '@remix-run/react';
 import NProgress from 'nprogress';
 import nprogressStyles from 'nprogress/nprogress.css';
 
@@ -16,19 +16,18 @@ export const links: LinksFunction = () => {
 const ConfNProgress = NProgress.configure({ showSpinner: false });
 
 const useNprogress = () => {
-
-  let transition = useTransition();
+  let navigation = useNavigation();
 
   useEffect(() => {
     // and when it's something else it means it's either submitting a form or
     // waiting for the loaders of the next location so we start it
-    if (transition.state !== "idle") {
+    if (navigation.state !== "idle") {
       ConfNProgress.start();
     } else {
       ConfNProgress.done();
     }
     // when the state is idle then we can to complete the progress bar
-  }, [transition.state]);
+  }, [navigation.state]);
 };
 
 export default useNprogress;
