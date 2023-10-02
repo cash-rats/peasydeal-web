7124cfe520ec80e47cfd54e1b8fa2e9dd077de8b
Bblurock <>
2023-10-02 21:58:28 +0800

    Update contact us with phone

--- app/routes/__index/contact-us.tsx
+++ app/routes/__index/contact-us.tsx
@@ -33,11 +33,10 @@ export default function AboutUs() {
           Contact Us
         </h1>
       </div>
-      <article className="peasydeal-v1 pt-8">
-        <p className="py-4">If you have any questions or concerns, please do not hesitate to contact us. We would love to hear from you, contact us on: contact@peasydeal.com</p>
-        <blockquote>Whilst every effort is made to respond to all messages as soon as possible, during busy periods this could take up to 3 working days.</blockquote>
+      <article className="peasydeal-v1 pt-8 rounded-xl border-2 p-8">
+        <p className="py-4">If you have any questions or concerns, please do not hesitate to contact us. We would love to hear from you, contact us via:</p>
         <section>
-          <h3 className="pt-4 text-lg">Email</h3>
+          <h3 className="pt-4 pb-0 text-lg">Email</h3>
           <p>
             <a
               className="flex flex-cols"
@@ -50,11 +49,26 @@ export default function AboutUs() {
         </section>
 
         <section>
-          <h3 className="pt-4 text-lg">Address</h3>
+          <h3 className="pt-4 pb-0 text-lg">Address</h3>
           <p>
             5th Floor 167 169 Great Portland Street, London, W1W 5PF, United Kingdom
           </p>
         </section>
+
+        <section>
+          <h3 className="pt-4 pb-0 text-lg">Phone</h3>
+          <p>
+            020 4577 3092
+          </p>
+        </section>
+
+        <blockquote className='text-left'>
+          <b>Opening Times: (excluding bank holidays)</b>
+          <p>Monday - Friday 9am - 5pm</p>
+          <p>Weekend - Closed</p>
+        </blockquote>
+
+        <small>Whilst every effort is made to respond to all messages as soon as possible, during busy periods this could take up to 3 working days.</small>
 
       </article>
     </div>
