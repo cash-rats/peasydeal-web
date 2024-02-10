de1a44b9af88cbcefa6058ba0437f09bdf1b9b05
Chi-Han Huang <huangchihan@gmail.com>
2024-02-10 12:14:29 +0800

    Remove unused code.

--- app/components/PromoCarousell/index.tsx
+++ app/components/PromoCarousell/index.tsx
@@ -19,45 +19,6 @@ const campaigns = [1, 2];
 
 const getEvent = (index: number) => {
   const campaign = [
-    // (
-    //   <Link
-    //     key={`campaign_card_${index}`}
-    //     to='/events/win-a-free-surprise-gift'
-    //     onClick={() => {
-    //       window.rudderanalytics?.track('click_event_carousell', {
-    //         event: 'win_a_free_surprise_gift',
-    //       });
-    //     }}
-    //   >
-    //     <div className="rounded-2xl bg-gradient-to-r from-red-500 to-orange-500 text-white h-[200px] flex justify-between p-4 self-center mx-2 gap-2">
-    //       <div className="flex self-center flex-col flex-1">
-    //         <h3 className="font-poppin font-medium  text-xl md:text-2xl lg:text-3xl">Win a surprise gift for your next purchase</h3>
-    //         <Button
-    //           colorScheme='whiteAlpha'
-    //           size="lg"
-    //           variant='solid'
-    //           className='capitalize text-lg md:text-xl mt-3 md:mt-4 bg-white text-orange-800 inline-flex w-[180px] md:w-[200px]'
-    //         >
-    //           I want free gifts
-    //         </Button>
-    //       </div>
-    //       <span className="flex-1 overflow-hidden">
-    //         <div
-    //           className='animate-scrollgrid'
-    //           style={{
-    //             height: "200px",
-    //             marginTop: "-12px",
-    //             backgroundSize: "1600px",
-    //             backgroundRepeat: "repeat-x",
-    //             backgroundPosition: "center",
-    //             width: "1775px",
-    //             backgroundImage: `url(${bg})`,
-    //           }} />
-    //       </span>
-    //     </div>
-    //   </Link>
-    // ),
-
     (
       <Link
         key={`campaign_card_${index}`}
