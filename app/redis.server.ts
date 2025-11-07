1d8dca906a88eacde1e0f068affd8994ae799885
Bryan Huang <huangchiheng@gmail.com>
2025-11-07 14:04:11 +0800

    Fix redis conn.

--- app/redis.server.ts
+++ app/redis.server.ts
@@ -1,22 +1,31 @@
-import type { RedisOptions, Redis } from 'ioredis';
-import IORedis from 'ioredis';
+import IORedis, {
+  type RedisOptions,
+  type Redis,
+} from 'ioredis';
+
+import { env, isProd } from '~/utils/env';
 
 let ioredis: Redis;
 let options: RedisOptions = {
-  port: Number(process.env.REDIS_PORT),
-  host: process.env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  host: env.REDIS_HOST,
+  username: env.REDIS_USER,
+  password: env.REDIS_PASSWORD,
+  db: env.REDIS_DB,
 }
+
 declare global {
   var __redis__: Redis;
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (isProd) {
   ioredis = new IORedis(options);
 } else {
   if (process.env.NODE_ENV === 'test') {
     options = {
       port: 6379,
       host: '127.0.0.1',
+      db: 0,
     }
   }
 
