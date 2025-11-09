c1dea4831f6430ad9651536e83be74058516352e
Bryan Huang <huangchiheng@gmail.com>
2025-11-09 23:41:14 +0800

    Minor tweak

--- app/sessions/redis_session.server.ts
+++ app/sessions/redis_session.server.ts
@@ -4,14 +4,14 @@ import { envs } from '~/utils/env';
 
 import { createRedisSessionStorage } from './create_redis_session.server';
 
-invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
+invariant(envs.SESSION_SECRET, "SESSION_SECRET must be set");
 
 const { getSession, commitSession, destroySession } = createRedisSessionStorage({
   cookie: {
     name: "__rsession",
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
-    secrets: [process.env.SESSION_SECRET],
+    secrets: [envs.SESSION_SECRET],
     path: "/",
     httpOnly: true,
     maxAge: envs.REDIS_SESSION_TTL,
