72d1f71b8a2c0048bf41d400ce569ba4b578bc19
Bryan Huang <huangchiheng@gmail.com>
2025-11-17 12:36:00 +0800

    Rewrite remix-image to react-router-image to fit my need

--- app/routes/remix-image/route.tsx
+++ app/routes/remix-image/route.tsx
@@ -1,6 +1,6 @@
 import type { LoaderFunctionArgs } from 'react-router';
-import { imageLoader, MemoryCache } from 'remix-image/server';
-import type { LoaderConfig } from 'remix-image';
+import { imageLoader, MemoryCache } from '@huangc28/react-router-image/server';
+import type { LoaderConfig } from '@huangc28/react-router-image';
 import httpStatus from 'http-status-codes';
 
 import { envs } from '~/utils/env';
