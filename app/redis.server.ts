3507061839d9d76b785f9f9d4ce5e7044273abe3
Bryan Huang <huangchiheng@gmail.com>
2025-11-23 00:53:18 +0800

    debug log

--- app/redis.server.ts
+++ app/redis.server.ts
@@ -9,6 +9,7 @@ declare global {
 // Initialize Redis instance with singleton pattern
 if (!global.__redis__) {
   const dsn = `rediss://${env.REDIS_USER}:${env.REDIS_PASSWORD}@${env.REDIS_HOST}:${env.REDIS_PORT}`;
+  console.log('~~ rediss', dsn)
   global.__redis__ = new IORedis(dsn);
 }
 
