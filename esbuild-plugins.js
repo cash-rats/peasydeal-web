de9d7c877187aef6cf705b4384980f0fcb8cf294
Bryan Huang <huangchiheng@gmail.com>
2026-01-13 13:40:47 +0800

    Remove unused file

--- esbuild-plugins.js
+++
@@ -1,8 +0,0 @@
-const styledComponentsPlugin = require('@lukalabs/esbuild-styled-components').default;
-
-exports.plugins = function(plugins) {
-  return [styledComponentsPlugin({
-    ssr: true,
-    namespace: 'peasydeal',
-  }), ...plugins];
-}
